8cde1ebdae79ea68d18585a2e0a66ac008792147
Sinedin <dndiaz8@misena.edu.co>
2021-01-07 16:21:02 -0500

    endpoint 3  final

--- src/routes/roulettes.js
+++ src/routes/roulettes.js
@@ -54,10 +54,10 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res)=>{
   const id_roulette = req.params.id;
-  let state, validate_user = [],bet__status = [], array_delete_bet = [], array_Outcom =[];
+  let state, validate_user = [],bet__status = [],array_result = [], array_Outcome=[];
   req.body.forEach((elem, index)=>
   {
-    if(elem.bet_value > 0 && elem.bet_value <= 10000)
+    if(elem.bet_value > 0 && elem.bet_value <= 10000 && elem.number >= 0 && elem.number <=36)
     {
       elem.bet_status = true;
     }
@@ -77,28 +77,36 @@ router.put('/:id', (req, res)=>{
         state = vals.state === "open" ? true : false;
       }
     });
-    console.log(req.body);
+    if(!state)
+    {
+      res.status(500).json({error: "Wrong Request!!! Roulette close."});
+    }
     req.body.forEach((val, i)=>
     {
       users.forEach(values => 
       {
-        if(val.id_user === values.id_user )
+        if(val.id_user === values.id_user && parseFloat(val.bet_value) <= parseFloat(values.credit))
         {
-          // console.log('numbers: ',val.bet_value, values.credit);
-          // console.log('i: ',i);
-          let bet_status = parseFloat(val.bet_value) <= parseFloat(values.credit) ? true: false;
-          bet__status.push()
-          // console.log('bet__status: ', bet__status);
+          validate_user.splice(validate_user.length,0,val.id_user);
         }
-        else
-        {
-          // console.log('index: ',i);
-          // console.log('error: ',val.id_user);
-        }
-        // bet_status
-      })
+      });
     });
-    res.json({resp: "true", state_roulete: state, bet_status: bet__status});
+    validate_user.forEach(val1 =>
+    {
+      req.body.forEach((val2,i) =>
+      {
+        if(val2.id_user == val1)
+        {
+          array_result.splice(array_result.length*5,0,val2);
+        }
+      });
+    });
+    if(array_result.length > 0)
+    {
+      bet__status = true;
+      bets.push(array_result);
+    }
+    res.json({N_roulette: id_roulette, bet_status: bet__status,roulette: roulette, array_result: array_result, all_bets: bets});
   }
   else
   {
